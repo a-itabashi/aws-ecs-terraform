fb6bd6a32565e18295ac670ef72262ee91679495
Aguru Itabashi <itabashi.a@gmail.com>
2025-03-27 10:22:25 +0900

    add network

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 terraform {
-  # Assumes s3 bucket and dynamo DB table already set up
   backend "s3" {
     bucket         = "aws-ecs-terraform-tfstate-sample-itabashi"
     key            = "terraform.tfstate"
@@ -7,4 +6,15 @@ terraform {
     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
     encrypt        = true
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
 }
